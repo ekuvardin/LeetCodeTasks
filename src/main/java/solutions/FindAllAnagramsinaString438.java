58887c3a8e8eb35f78cce4adf2314ef1c71770cc
Evgeniy Kuvardin <evgeniy.kuvardin@gmail.com>
2025-07-01 23:46:12 +0300

    Add some cases

--- src/main/java/solutions/FindAllAnagramsinaString438.java
+++ src/main/java/solutions/FindAllAnagramsinaString438.java
@@ -5,6 +5,37 @@ import java.util.Arrays;
 import java.util.List;
 
 /*
+Given two strings s and p, return an array of all the start indices of p's
+
+in s. You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+
+Example 2:
+
+Input: s = "abab", p = "ab"
+Output: [0,1,2]
+Explanation:
+The substring with start index = 0 is "ab", which is an anagram of "ab".
+The substring with start index = 1 is "ba", which is an anagram of "ab".
+The substring with start index = 2 is "ab", which is an anagram of "ab".
+
+
+
+Constraints:
+
+    1 <= s.length, p.length <= 3 * 104
+    s and p consist of lowercase English letters.
+
+
 yandex
  */
 public class FindAllAnagramsinaString438 {
@@ -35,8 +66,7 @@ public class FindAllAnagramsinaString438 {
                 result.add(i - p.length() + 1);
             }
 
-            index = s.charAt(i - p.length() + 1) - 'a';
-            letters[index] = letters[index] - 1;
+            letters[s.charAt(i - p.length() + 1) - 'a']--;
         }
 
         return result;
